886c621bc3a1b43f85f7061b02face0ff95b15ac
klequis <klequis@gmail.com>
2018-07-28 21:23:01 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CustomMenu from './CustomMenu'
+import ManageMenuVisibility from './ManageMenuVisibility'
+
+const App = () => {
+  return (
+    <div>
+      <CustomMenu />
+      <ManageMenuVisibility />
+    </div>
+  )
+}
+
+export default App
